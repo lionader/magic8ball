4bf08be0114944dcb5a16791c88859c9857c0c9c
Nader <nnayeri@gmail.com>
2020-01-01 10:32:18 +0330

    updated static link static rout

--- magic8ball/settings.py
+++ magic8ball/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = "/home/redan/redan.pythonanywhere.com/static/"
 
-STATIC_URL = '/home/redan/redan.pythonanywhere.com/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
